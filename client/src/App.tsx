b6efc7e39990bb6de676aabb7e0d52108cb009d0
Huzaifa Ansari <huzaifaks123@gmail.com>
2024-10-17 13:43:14 +0530

    boiler plate ready

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings),[])
+  return <div className="app">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      hii
+    </ThemeProvider>
+  </div>
+}
+
+export default App
